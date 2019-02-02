496707d69c5723d8e1c743fd57cdd0ad2148aa41
Sherief Farid <sherief@farid.cloud>
2019-02-02 02:24:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,24 @@
 function add(a,b) {
   return a+b
 }
+
+function subtract(a,b){
+  return b-a
+}
+
+function multiply (a,b){
+  return a*b
+}
+
+function divide (a,b){
+  return a/b
+}
+
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n-- 
+}
+
